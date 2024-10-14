5d2bbae44af95c3c3f86c87a921d824bc81521e5
Malte <malte.roevemeier@ge.com>
2024-10-14 12:07:02 +0200

    Klausur Übung

---
+++ Klausur/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Klausur
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestBoote();
+        }
+
+        private void TestBoote()
+        {
+            Random random = new Random();
+            List<Sportboot> boote = new List<Sportboot>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                int laenge = random.Next(5, 11); // Länge zwischen 5 und 10 Metern
+                int gewicht = 275 * laenge - 1125; // Gewicht nach der Funktion g(x)=275*x-1125
+
+                if (random.Next(2) == 0)
+                {
+                    // Motorboot
+                    int motorleistung = random.Next(5, 21); // Motorleistung zwischen 5 und 20 kW
+                    boote.Add(new Motorboot(laenge, gewicht, motorleistung));
+                }
+                else
+                {
+                    // Segelboot
+                    int segelflaeche = random.Next(5, 21); // Segelfläche zwischen 5 und 20 m²
+                    boote.Add(new Segelboot(laenge, gewicht, segelflaeche));
+                }
+            }
+
+            List<Sportboot> fuehrerscheinfreieBoote = new List<Sportboot>();
+            List<Sportboot> fuehrerscheinpflichtigeBoote = new List<Sportboot>();
+
+            foreach (var boot in boote)
+            {
+                if (boot is Motorboot motorboot)
+                {
+                    if (motorboot.Fuehrerscheinpflichtig)
+                    {
+                        fuehrerscheinpflichtigeBoote.Add(motorboot);
+                    }
+                    else
+                    {
+                        fuehrerscheinfreieBoote.Add(motorboot);
+                    }
+                }
+                else if (boot is Segelboot segelboot)
+                {
+                    if (segelboot.Fuehrerscheinpflichtig)
+                    {
+                        fuehrerscheinpflichtigeBoote.Add(segelboot);
+                    }
+                    else
+                    {
+                        fuehrerscheinfreieBoote.Add(segelboot);
+                    }
+                }
+            }
+
+            // Annahme: listBoxFuehrerscheinfrei und listBoxFuehrerscheinpflichtig sind ListBox-Elemente im XAML
+            listBoxFuehrerscheinfrei.ItemsSource = fuehrerscheinfreieBoote;
+            listBoxFuehrerscheinpflichtig.ItemsSource = fuehrerscheinpflichtigeBoote;
+        }
+    }
+}
---
+++ Klausur/Sportboot.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Klausur
+{
+    internal class Sportboot
+    {
+        private readonly int laenge;
+        private readonly int gewicht;
+
+        public int Laenge
+        {
+            get { return laenge; }
+        }
+
+        public double LaengeInFuss
+        {
+            get { return laenge / 0.3048; }
+        }
+
+        public int Gewicht
+        {
+            get { return gewicht; }
+        }
+
+        public Sportboot(int laenge, int gewicht)
+        {
+            if (laenge < 5 || laenge > 10)
+            {
+                throw new ArgumentOutOfRangeException("laenge", "Die Bootslänge muss zwischen 5m und 10m liegen.");
+            }
+            this.laenge = laenge;
+            this.gewicht = gewicht;
+        }
+
+        public override string ToString()
+        {
+            return $"Die Länge des Sportbootes beträgt {laenge} Meter ({LaengeInFuss:F2} Fuß) und das Gewicht beträgt {gewicht} kg.";
+        }
+    }
+    internal class Motorboot : Sportboot
+    {
+        private readonly int motorleistung;
+
+        public int Motorleistung
+        {
+            get { return motorleistung; }
+        }
+
+        public bool Fuehrerscheinpflichtig
+        {
+            get { return motorleistung > 11; }
+        }
+
+        public Motorboot(int laenge, int gewicht, int motorleistung) : base(laenge, gewicht)
+        {
+            this.motorleistung = motorleistung;
+        }
+
+        public override string ToString()
+        {
+            return $"Das Motorboot hat eine Länge von {Laenge} Meter ({LaengeInFuss:F2} Fuß), ein Gewicht von {Gewicht} kg und eine Motorleistung von {Motorleistung} kW. Führerscheinpflichtig: {Fuehrerscheinpflichtig}";
+        }
+    }
+
+    internal class Segelboot : Sportboot
+    {
+        private readonly int segelflaeche;
+        public int Segelflaeche
+        {
+            get { return segelflaeche; }
+        }
+
+        public bool Fuehrerscheinpflichtig
+        {
+            get { return segelflaeche > 12; }
+        }
+
+        public Segelboot(int laenge, int gewicht, int segelflaeche) : base(laenge, gewicht)
+        {
+            this.segelflaeche = segelflaeche;
+        }
+
+        public override string ToString()
+        {
+            return $"Das Motorboot hat eine Länge von {Laenge} Meter ({LaengeInFuss:F2} Fuß), ein Gewicht von {Gewicht} kg und eine Segelfläche von {Segelflaeche} M. Führerscheinpflichtig: {Fuehrerscheinpflichtig}";
+        }
+    }
+}
